de45af56f43f833513a056fccf2361a49861fdd0
kevin <kevinphilip419@gmail.com>
2026-06-24 15:00:47 -0500

    clean: remove duplicate nested repistory folder

--- harper-geoguesser/server.js
+++
@@ -1,7 +0,0 @@
-  firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
-            console.log("User already authenticated. Passing to game...");
-            // Corrected to main.html so auto-login doesn't break
-            window.location.href = "main.html";
-        }
-    });
